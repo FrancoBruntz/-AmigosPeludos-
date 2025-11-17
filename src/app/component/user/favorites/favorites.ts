88b392ae8559f03981a97e6029c684fa4cbee99c
pritapparo <pritapparo@gmail.com>
2025-11-17 09:26:46 -0300

    update Favorites

--- src/app/component/user/favorites/favorites.ts
+++ src/app/component/user/favorites/favorites.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FavoriteService } from '../../../services/favorite.service';
 import Pets from '../../../models/pets';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../auth/auth-service';
 
 @Component({
   selector: 'app-favorites',
@@ -13,12 +14,32 @@ import { RouterLink } from '@angular/router';
 export class FavoritesComponent {
 
   favorites: Pets[] = [];
+  errorMessage = '';
 
-  constructor(private favoriteService: FavoriteService) {}
+  constructor(
+    private favoriteService: FavoriteService,
+    private auth: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+
+    // Si NO está logueado
+    if (!this.auth.isLogIn()) {
+      this.router.navigate(['/login']);
+      return;
+    }
+
+    // Si es ADMIN, redirigir a home
+    if (this.auth.isAdmin()) {
+      this.router.navigate(['/']);
+      return;
+    }
+
+    // Usuario normal sí puede ver sus favoritos
     this.favorites = this.favoriteService.getFavorites();
   }
+
 
   remove(id: string) {
     this.favoriteService.removeFavorite(id);
